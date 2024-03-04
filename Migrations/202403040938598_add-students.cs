5c8195b18abbb73ac8926c4b845afba4fd512f23
Fady <fady33zaher@gmail.com>
2024-03-04 12:28:07 +0200

    Get Rooms

---
+++ Migrations/202403040938598_add-students.cs
@@ -0,0 +1,26 @@
+﻿namespace University.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addstudents : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Students (StudentName, StudentNumber) VALUES ('Hassan', 1)");
+            Sql("INSERT INTO Students (StudentName, StudentNumber) VALUES ('Khalid', 2)");
+            Sql("INSERT INTO Students (StudentName, StudentNumber) VALUES ('Ali', 3)");
+            Sql("INSERT INTO Students (StudentName, StudentNumber) VALUES ('Mona', 4)");
+            Sql("INSERT INTO Students (StudentName, StudentNumber) VALUES ('Asmaa', 5)");
+            Sql("INSERT INTO Students (StudentName, StudentNumber) VALUES ('Hany', 6)");
+            Sql("INSERT INTO Students (StudentName, StudentNumber) VALUES ('Nada', 7)");
+            Sql("INSERT INTO Students (StudentName, StudentNumber) VALUES ('Sara', 8)");
+            Sql("INSERT INTO Students (StudentName, StudentNumber) VALUES ('Nada', 9)");
+            Sql("INSERT INTO Students (StudentName, StudentNumber) VALUES ('Ahmed', 10)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
